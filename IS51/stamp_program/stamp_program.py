20183e7a6a4147c2cc3f4608330253a70097c993
Andres C. Perez <Titas23@Andress-MacBook-Air.local>
2022-02-01 11:30:09 -0800

    update

---
+++ IS51/stamp_program/stamp_program.py
@@ -0,0 +1,15 @@
+
+def calculate(sheet):
+    answer = sheet / 5
+    rounded = round(answer, 1)
+    print("sheet is : ", sheet)
+    print("The answer is: ", answer)
+    print("rounded is: ", rounded)
+    print("")
+    return rounded
+
+
+calculate(16)
+calculate(65)
+calculate(88)
+calculate(43)
